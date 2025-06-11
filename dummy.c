72b99ab3d12f75d53dd788fcb5d8cc8df784e97b
Emerson Sales <emerson.sales@gransassotech.org>
2025-06-11 15:23:57 +0200

    initial commit

---
+++ dummy.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/* forward declarations */
+int  f1(void);
+void f2(int a, int b);
+void f3(void);
+void f4(void);
+void f6 (int x);
+void f7(int x);
+void f8(void);
+
+int main (void)
+{
+    int x = f1();
+    f6 (x);
+    return 0;
+}
+
+
+int f1(void)
+{
+    f2(0, 0);
+    return 0;
+}
+
+void f2(int a, int b)
+{
+    if (a > b || a < 10)
+    {
+        f1();
+    }
+    f3();
+    puts("blah");
+}
+
+void f3(void)
+{
+    f4();
+}
+
+void f4(void)
+{
+    /* no-op */
+}
+
+void f6(int x)
+{
+    x = x + 1;
+    f7(x);
+}
+
+void f7(int x)
+{
+    x = 3;
+    f8();
+}
+
+void f8(void)
+{
+    f6(0); 
+}
